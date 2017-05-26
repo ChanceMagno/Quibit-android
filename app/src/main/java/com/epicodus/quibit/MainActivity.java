3978d24e316144b6ef1bdcfb85c4bce20e2d58eb
Chance <Chancemagno@gmail.com>
2017-05-26 13:23:05 -0700

    removed createGoal activity and replaced, found template rendering issue

--- app/src/main/java/com/epicodus/quibit/MainActivity.java
+++ app/src/main/java/com/epicodus/quibit/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(MainActivity.this, CreateGoalActivity.class);
+        Intent intent = new Intent(MainActivity.this, CreateGoal.class);
         startActivity(intent);
     }
 
