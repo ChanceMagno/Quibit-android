3793a2dde805f23f987a88f026ef38f2f2957cd5
Chance <Chancemagno@gmail.com>
2017-05-26 12:54:40 -0700

    add CreateGoalActivity, GetStartButton onClick method activates intent to load CreateGoalActivity

--- app/src/main/java/com/epicodus/quibit/MainActivity.java
+++ app/src/main/java/com/epicodus/quibit/MainActivity.java
@@ -1,17 +1,40 @@
 package com.epicodus.quibit;
 
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.getStartedButton) Button mGetStartedButton;
+    boolean goalSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+        //for displaying getStarted button if no goal set
+        if (goalSet) {
+            mGetStartedButton.setVisibility(View.GONE);
+        } else {
+            mGetStartedButton.setOnClickListener(this);
+        }
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, CreateGoalActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
