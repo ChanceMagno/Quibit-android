398e1614863cc8624d8d456566f31ccfc39d004d
Chance <Chancemagno@gmail.com>
2017-05-26 10:43:40 -0700

    add assets/fonts folder, add pacifico.ttf font to fonts folder

--- app/src/main/java/com/epicodus/quibit/MainActivity.java
+++ app/src/main/java/com/epicodus/quibit/MainActivity.java
@@ -1,13 +1,17 @@
 package com.epicodus.quibit;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
