052a647c7e2927d14a19b6b1c3876a3e78d74251
Chance <Chancemagno@gmail.com>
2017-05-26 15:36:25 -0700

    Bind all Buttons, TextViews and EditText in createGoalActivity

--- app/src/main/java/com/epicodus/quibit/CreateGoal.java
+++ app/src/main/java/com/epicodus/quibit/CreateGoal.java
@@ -2,12 +2,34 @@ package com.epicodus.quibit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class CreateGoal extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateGoal extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.nextGoalButton) Button mNextGoalButton;
+    @Bind(R.id.cancelGoalButton) Button mCancelGoalButton;
+
+    @Bind(R.id.editTextName) EditText mEditTextName;
+    @Bind(R.id.editTextItem) EditText mEditTextItem;
+    @Bind(R.id.editTextItemCost) EditText mEditTextItemCost;
+    @Bind(R.id.editTextItemMonthly) EditText mEditTextItemMonthly;
+
+    @Bind(R.id.textViewName) TextView mTextViewName;
+    @Bind(R.id.textViewItem) TextView mTextViewItem;
+    @Bind(R.id.textViewItemCost) TextView mTextViewItemCost;
+    @Bind(R.id.textViewItemMonthly) TextView mTextViewItemMonthly;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_goal);
+        ButterKnife.bind(this);
     }
 }
