d34df0bbd367cf7e8d29d1280d8abf9517a4af45
Chance <Chancemagno@gmail.com>
2017-05-29 12:25:09 -0700

    add setItemAsGoalButton to item detail activity, create onclick listener for button

--- app/src/main/java/com/epicodus/quibit/ItemDetail.java
+++ app/src/main/java/com/epicodus/quibit/ItemDetail.java
@@ -3,14 +3,16 @@ package com.epicodus.quibit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ItemDetail extends AppCompatActivity {
-    @Bind(R.id.itemTextView)
-    TextView mItemTextView;
+public class ItemDetail extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.itemTextView) TextView mItemTextView;
+    @Bind(R.id.setItemAsGoalButton) Button mSetItemAsGoalButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,14 @@ public class ItemDetail extends AppCompatActivity {
         Intent intent = getIntent();
         String selectedItem = intent.getStringExtra("selectedItem");
         mItemTextView.setText(String.format("item detail here %s", selectedItem));
+
+        mSetItemAsGoalButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
