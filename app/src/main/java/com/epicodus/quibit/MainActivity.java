26ceba865d4efbfd6e7843347a54a4bf4c58bb18
Chance <Chancemagno@gmail.com>
2017-05-29 18:12:26 -0700

    add about floating action button to main activity, add listener to view about activity

--- app/src/main/java/com/epicodus/quibit/MainActivity.java
+++ app/src/main/java/com/epicodus/quibit/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.quibit;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.getStartedButton) Button mGetStartedButton;
     @Bind(R.id.getStartedText) TextView mGetStartedText;
     @Bind(R.id.goalSearch) EditText mGoalSearch;
+    @Bind(R.id.about) FloatingActionButton mAbout;
     boolean goalSet = false;
 
     @Override
@@ -28,15 +30,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+
         Typeface pacifico = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
         Typeface salsa = Typeface.createFromAsset(getAssets(), "fonts/Salsa.ttf");
         mGetStartedText.setTypeface(pacifico);
         mGetStartedButton.setTypeface(salsa);
 
+        mAbout.setOnClickListener(this);
+
         Intent intent = getIntent();
-         boolean goalstatus = Boolean.parseBoolean(intent.getStringExtra("goalSet"));
-            if(goalstatus){
-                goalSet = goalstatus;
+         boolean goalStatus = Boolean.parseBoolean(intent.getStringExtra("goalSet"));
+            if(goalStatus){
+                goalSet = goalStatus;
                 mGetStartedButton.setVisibility(View.GONE);
                 mGetStartedButton.setVisibility(View.GONE);
                 mGoalSearch.setVisibility(View.GONE);
@@ -49,20 +55,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //getStartedButton clicked activates search
     @Override
     public void onClick(View v) {
-        String goalSearch = mGoalSearch.getText().toString();
-        if (goalSearch.equals("")) {
-            Toast toast = Toast.makeText(MainActivity.this, "Please input an item to search for.", Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.CENTER, 0, 100);
-            toast.show();
-        } else {
-            mGoalSearch.setText("");
-            Intent intent = new Intent(MainActivity.this, SearchRewards.class);
-            intent.putExtra("goalSearch", goalSearch);
-            startActivity(intent);
+        switch (v.getId()) {
+            case R.id.getStartedButton:
 
+                String goalSearch = mGoalSearch.getText().toString();
+                if (goalSearch.equals("")) {
+                    Toast toast = Toast.makeText(MainActivity.this, "Please input an item to search for.", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 100);
+                    toast.show();
+                } else {
+                    mGoalSearch.setText("");
+                    Intent intent = new Intent(MainActivity.this, SearchRewards.class);
+                    intent.putExtra("goalSearch", goalSearch);
+                    startActivity(intent);
+
+                }
+                break;
+
+            case R.id.about:
+                Intent intentAbout = new Intent(MainActivity.this, About.class);
+                startActivity(intentAbout);
+                break;
         }
-
-
     }
-
 }
+
