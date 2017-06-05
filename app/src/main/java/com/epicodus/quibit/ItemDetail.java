cbc0df4c2b4223369aa767ad8c02e6b07808005e
Chance <Chancemagno@gmail.com>
2017-06-04 20:32:00 -0700

    add textviews, buttons, imageview to itemdetail activity and binded/set text to selected item set values

--- app/src/main/java/com/epicodus/quibit/ItemDetail.java
+++ app/src/main/java/com/epicodus/quibit/ItemDetail.java
@@ -1,24 +1,30 @@
 package com.epicodus.quibit;
 
-import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.epicodus.quibit.models.Item;
 
 import org.parceler.Parcels;
+import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ItemDetail extends AppCompatActivity {
-    @Bind(R.id.itemTextView) TextView mItemTextView;
-    @Bind(R.id.setItemAsGoalButton) Button mSetItemAsGoalButton;
+public class ItemDetail extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.itemNameTextView) TextView mItemNameTextView;
+    @Bind(R.id.setGoalActionButton) FloatingActionButton mSetItemAsGoalButton;
+    @Bind(R.id.itemImageView) ImageView mItemImageView;
+    @Bind(R.id.itemDescriptionTextView) TextView mItemDescriptionTextView;
+    @Bind(R.id.itemViewOnlineActionButton) FloatingActionButton mItemViewOnlineButton;
+    @Bind(R.id.itemPriceTextView) TextView mItemPriceTextView;
     Item selectedItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +33,17 @@ public class ItemDetail extends AppCompatActivity {
 
         selectedItem = Parcels.unwrap(getIntent().getParcelableExtra("item"));
 
+        mItemNameTextView.setText(selectedItem.getName());
+        mItemDescriptionTextView.setText(selectedItem.getDescription());
+        mItemPriceTextView.setText(selectedItem.getSalePrice());
 
 
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
+
+
