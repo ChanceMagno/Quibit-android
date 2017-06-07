22ac80da5d08f33a272b7f5d5b6e542f2d5ccb07
Guest User <Guest@Epicodus-5F.local>
2017-06-07 09:59:20 -0700

    add user authentication for current users to log in

--- app/src/main/java/com/epicodus/quibit/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/quibit/ui/LoginActivity.java
@@ -1,15 +1,101 @@
 package com.epicodus.quibit.ui;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.quibit.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-    @Override
+import static android.widget.Toast.makeText;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.emailLoginEditText) EditText mEmailLoginEditText;
+    @Bind(R.id.passwordLoginEditText) EditText mPasswordLoginEditText;
+    @Bind(R.id.loginFloatingActionButton) FloatingActionButton mLoginButton;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        //listener checking login status of user
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+          @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+              FirebaseUser user = firebaseAuth.getCurrentUser();
+              if(user !=null){
+                  Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                  intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                  startActivity(intent);
+                  finish();
+              }
+          }
+        };
+
+        mLoginButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onClick(View view){
+        if(view == mLoginButton){
+            logInExistingUser();
+        }
+    }
+
+    public void logInExistingUser(){
+        String email = mEmailLoginEditText.getText().toString().trim();
+        String password = mPasswordLoginEditText.getText().toString().trim();
+        if(email.equals("")){
+            mEmailLoginEditText.setError("Please enter an email to login");
+            return;
+        }
+        if(password.equals("")){
+            mPasswordLoginEditText.setError("Please enter a password");
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
 }
