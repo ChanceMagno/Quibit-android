1182d0901bc969c2a20051a201926444d1b7ca9c
chance <chancemagno@gmail.com>
2017-06-09 12:32:35 -0700

    corrected inflater not setting correct layouts

--- app/src/main/java/com/epicodus/quibit/fragments/tab2Fragment.java
+++ app/src/main/java/com/epicodus/quibit/fragments/tab2Fragment.java
@@ -16,7 +16,7 @@ public class tab2Fragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab1_fragment, container, false);
+        View view = inflater.inflate(R.layout.tab2_fragment, container, false);
         return view;
     }
 }
