5a8ccd0479cabde57bde0fb1175aca66cd82787a
chance <chancemagno@gmail.com>
2017-06-19 21:49:34 -0700

    add landscape oreintation to create account layout

--- app/src/main/java/com/epicodus/quibit/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/quibit/ui/CreateAccountActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +13,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epicodus.quibit.R;
-import com.epicodus.quibit.models.UserProfile;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,7 +38,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_accont);
+        setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
 
         mAuth = FirebaseAuth.getInstance();
